443f90f83ccd6fc8011c3172c72bf4ee8792784f
avanipritha <105854907+avanipritha@users.noreply.github.com>
2023-03-17 11:45:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+moustacheX=0;
+moustacheY=0;
+
+function preload() {
+ moustache = loadImage('https://i.postimg.cc/y6Z5FVXx/moustacheimage.png')
+}
+
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('poseNet Is Initialized');
+}
+
+function draw() {
+    image(video,0,0,300,300);
+    image(moustache, moustacheX, moustacheY, 60, 40);
+} 
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x : " + results[0].pose.nose.x);
+        moustacheX = results[0].pose.nose.x - 30;
+        console.log("nose y : " + results[0].pose.nose.y);
+        moustacheY = results[0].pose.nose.y; 
+    }
+}
+
